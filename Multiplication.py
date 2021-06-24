fea733f9ae79c1c54148832da8552bc6632a0dfe
Hakan ATEŞLİ <hakaatesli9000@gmail.com>
2021-06-24 13:24:22 +0300

    Multiplication Class Has Created.

---
+++ Multiplication.py
@@ -0,0 +1,40 @@
+import math
+
+
+class Multiplication:
+
+    def __init__(self,matrix1,matrix2):
+        self.matrix1 = matrix1
+        self.matrix2 = matrix2
+        self.result = []
+        
+
+    def ismultiplyable(self):
+        """
+        INPUT: No Input
+        OUTPUT: ismultiplyable : bool
+
+        DESC: Can matrices be multiplied?
+        """
+        if len(self.matrix1) == 0 or len(self.matrix2) == 0:
+            return False
+        else:
+            return True
+    
+    def make_result(self,col,row):
+        """
+        INPUT: col: int
+               row: int
+        OUTPUT: result: list
+
+        DESC: Creates a col x row dimensional zero matrix.
+        """
+        self.result=[[0 for i in range(col)] for j in range(row)] 
+        return self.result
+
+    def display_result(self):
+        """
+        DESC: Display result matrix
+        """
+        for r in self.result:
+            print(r)
